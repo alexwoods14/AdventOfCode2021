c6b478da5bd586de9ece15e683a89a6f2c92fd8b
Alex Woods <alex.woods.1404@gmail.com>
2021-12-06 16:02:12 +0000

    day 6, nice and easy

---
+++ 6/day6.f90
@@ -0,0 +1,41 @@
+program day6
+  implicit none
+
+  integer, allocatable, dimension(:) :: input
+  integer(kind=8), dimension(0:8) :: days, new
+
+  integer :: ierr, N, i
+  character :: A
+
+  open(1,file="input.txt")
+  N=1
+  do
+    read(1,'(A)',advance="no",iostat=ierr) A
+    if (ierr.NE.0) exit
+    if(A .EQ. ',') N=N+1
+  end do
+  rewind(1) ! back to top
+
+  allocate(input(N))
+  read(1, *) input
+
+  new = 0
+  days = 0
+
+
+  do i = 0, 8
+    days(i) = count(input == i)
+  end do
+
+  do i = 1, 256
+    new(0:7) = days(1:8)
+    new(6) = new(6) + days(0)
+    new(8) = days(0)
+    days = new
+  end do
+
+  write(*,*) sum(days)
+
+
+
+end program day6

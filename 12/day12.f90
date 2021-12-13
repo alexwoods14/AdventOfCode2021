2b3ebc660536d4a8fb6acda4188cb343fe8ac23d
Alex Woods <alex.woods.1404@gmail.com>
2021-12-13 12:12:54 +0000

    no cleaner but not takes 0.07 seconds not 2

--- 12/day12.f90
+++ 12/day12.f90
@@ -9,6 +9,7 @@ program day12
   character*5, allocatable, dimension(:) :: stack
   character*10 :: A
   character*100 :: route
+  integer, allocatable, dimension(:) :: counts
 
   integer :: ierr, i, j, N, M, ENDN, STARTN, loc, sPtr, num, maxpaths
 
@@ -35,6 +36,7 @@ program day12
   allocate(input(N))
   allocate(stack(N+5))
   allocate(caveids(N))
+  allocate(counts(N))
   allocate(connections(N,N))
 
   ! create pointer for each one
@@ -55,6 +57,7 @@ program day12
   num = 0
   stack = ''
   sPtr = 0
+  counts = 0
   call pathspart2(stack, sum(findloc(caveids,"start")))
 
   write(*,*) "Part 2:", num
@@ -63,6 +66,7 @@ program day12
   
   deallocate(input)
   deallocate(caveids)
+  deallocate(counts)
   deallocate(connections)
   deallocate(stack)
 
@@ -113,30 +117,20 @@ contains
     else
     do i = 1, N 
       if (connections(i,id) == .true.) then ! if connected
-        if(sum(findloc(stack, caveids(i))) == 0 .or. is_cap(caveids(i)(1:1)) .or. (.not.  aretwolowercase(stack) .and. caveids(i) /= "start" .and. caveids(i) /= "end")) then
+        if(sum(findloc(stack, caveids(i))) == 0 .or. is_cap(caveids(i)(1:1)) .or. (maxval(counts) < 2 .and. caveids(i) /= "start" .and. caveids(i) /= "end")) then
+            if(.not. is_cap(caveids(i)(1:1)) .and. caveids(i) /= "start" .and. caveids(i) /= "end") then
+              counts(i) = counts(i) + 1
+            end if
             call pathspart2(stack, i)
             stack(sPtr) = ''
             sPtr = sPtr -1
+            counts(i) = counts(i) - 1
         end if
       end if
     end do
     end if
 
   end subroutine pathspart2
-  
-  function aretwolowercase(stack)
-    character*5, allocatable, dimension(:) :: stack
-    integer :: i
-    logical :: aretwolowercase
-
-    aretwolowercase = .false.
-    do i = 1, count(caveids /= '')
-      if (caveids(i) /= "start" .and. caveids(i) /= "end" .and. .not. is_cap(caveids(i)(1:1))) then
-        aretwolowercase = aretwolowercase .or. count(stack == caveids(i)) >= 2
-      end if
-    end do
-
-  end function aretwolowercase
 
   function is_cap(c)
     character :: c
